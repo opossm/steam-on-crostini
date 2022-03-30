c3830ba17d9a8434bd642201f052cb5cf23e212a
yeenyeenyeen <72998497+yeenyeenyeen@users.noreply.github.com>
2022-03-30 13:14:20 -0500

    Update source-fix.sh
    slightly rewritten

--- source-fix.sh
+++ source-fix.sh
@@ -1,10 +1,12 @@
 clear
-Source Fix for Crostini v0.2 by StingyMaxdog
+Source Fix for Crostini v0.3 by StingyMaxdog/Maxintosh
+sudo apt install gedit -y 
+sudo cd /usr/share/applications
+sudo chmod a+rwx steam.desktop
+sudo cp /usr/share/applications/steam.desktop ~/.local/share/applications
+cd ~/.local/share/applications
 sudo echo ====================
 sudo echo Replace the current steam exec with 'Exec=env VIRGL_DEBUG=bgraswz steam' then save. Only do this for the first chunk of text, and nowhere else.
 sudo echo AFTER YOU HAVE MODIFIED AND SAVED THIS FILE, RESTART LINUX FOR IT TO WORK!!!
 sudo echo ====================
-sudo apt install nano -y
-sudo cp /usr/share/applications/steam.desktop ~/.local/share/applications
-cd ~/.local/share/applications
-sudo nano steam.desktop
+gedit steam.desktop
