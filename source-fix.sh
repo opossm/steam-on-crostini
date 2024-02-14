5f5108dbbce425361a9581b690ea046f5166dc4d
maxintosh <72998497+opossm@users.noreply.github.com>
2024-02-14 02:59:44 -0600

    Update source-fix.sh
    Removed functionality

--- source-fix.sh
+++ source-fix.sh
@@ -1,12 +1,1 @@
-clear
-Source Fix for Crostini v0.3 by StingyMaxdog/Maxintosh
-sudo apt install gedit -y 
-sudo cd /usr/share/applications
-sudo chmod a+rwx steam.desktop
-sudo cp /usr/share/applications/steam.desktop ~/.local/share/applications
-cd ~/.local/share/applications
-sudo echo ====================
-sudo echo Replace the current steam exec with 'Exec=env VIRGL_DEBUG=bgraswz steam' then save. Only do this for the first chunk of text, and nowhere else.
-sudo echo AFTER YOU HAVE MODIFIED AND SAVED THIS FILE, RESTART LINUX FOR IT TO WORK!!!
-sudo echo ====================
-gedit steam.desktop
+echo This script is not needed anymore, check the README.md for fixing colors.
