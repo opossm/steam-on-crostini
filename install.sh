0a323dd960d3e935f41498229cc5596539c57115
maxintosh <72998497+opossm@users.noreply.github.com>
2024-02-14 02:47:26 -0600

    Update install.sh
    Removed mention of fix script

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ sudo dpkg --add-architecture i386
 sudo chmod a+rwx /etc/apt/sources.list && sudo echo 'deb http://deb.debian.org/debian/ stable main contrib non-free' > /etc/apt/sources.list
 sudo apt update && sudo apt upgrade -y
 sudo apt install steam -y
-echo Please launch and set up Steam before using the Source Fix script
+echo Steam is now installed
+echo Check https://github.com/opossm/steam-on-crostini for any updates and fixes.
