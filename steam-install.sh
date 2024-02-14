38accff37b70a47500ea1a38599af27d62341e4c
maxintosh <72998497+opossm@users.noreply.github.com>
2024-02-14 03:01:19 -0600

    Update steam-install.sh
    Removed functionality

--- steam-install.sh
+++ steam-install.sh
@@ -1,10 +1,1 @@
-clear
-sudo echo Steam Installation for Crostini v0.3 by StingyMaxdog
-sudo echo ====================
-sudo echo Would you like to install Steam? Type 1 or 2
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) ./install.sh;;
-        No ) exit;;
-    esac
-done
+echo This script is not needed anymore, check the README.md for more info.
